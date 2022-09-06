06625873e1a79b81da523dafb787c4d6de719464
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-09-06 17:26:49 +0800

    Minor changes on sign up

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -6,7 +6,6 @@ import "./signUp.scss";
 const SignUp = () => {
   return (
     <div>
-      <Link to="/">Forgot Password?</Link>
       <Link to="/">Already have an account?</Link>
       <h1>Sign Up</h1>
       <SignUpForm />
