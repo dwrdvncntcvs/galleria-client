9736037caa3f34a05a7c09973ccae1f4717dcfa6
Reneil Moncawe <reneil.moncawe@g.batstate-u.edu.ph>
2022-09-06 17:40:13 +0800

    signinForm Fields

--- src/pages/SignIn/components/SignInForm/SignInForm.tsx
+++ src/pages/SignIn/components/SignInForm/SignInForm.tsx
@@ -1,8 +1,45 @@
-import React from "react";
+import React, {useState, ChangeEvent, FormEvent} from "react";
 import "./signInForm.scss";
 
 const SignInForm = () => {
-  return <div>SignInForm</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const inputFields =[
+    {
+      placeholder: "Email",
+      type: "email",
+      value: email,
+      onChange: (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)
+    },
+    {
+      placeholder: "Password",
+      type: "password",
+      value: password,
+      onChange: (e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)
+    }
+  ];
+
+
+  const submit = (e: FormEvent) => {
+    e.preventDefault()
+    const data = {email, password}
+    console.log(data)
+  };
+  return <div>SignInForm
+      <h1>Sign In</h1>
+        <form onSubmit={submit}>
+          {inputFields.map(({placeholder, type, value, onChange}) =>(
+            <input 
+            placeholder={placeholder}
+            type={type}
+            value={value}
+            onChange={onChange}
+            />
+          ))}
+          <button className="s__button" type="submit">Sumbit</button>
+        </form>
+  </div>;
 };
 
 export default SignInForm;
