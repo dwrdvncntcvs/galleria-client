a5ad8001c8057bf6727678e02f2ff8a168bb3e76
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-09-10 16:13:20 +0800

    NavBar component initial changes

--- src/components/global/NavBar/NavBar.tsx
+++ src/components/global/NavBar/NavBar.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-import { useAppSelector } from "../../../hooks/reduxHook";
+import { signOutRequest } from "../../../api/userRequest";
+import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHook";
 import "./navBar.scss";
 
 export default function NavBar() {
   const { userState } = useAppSelector((state) => state);
+  const dispatch = useAppDispatch();
 
-  return userState.isAuth ? <div>NavBar</div> : null;
+  return userState.isAuth ? (
+    <div className="nb__main-container">
+      <section className="nb__content-container">
+        <h1>Galleria</h1>
+        <nav></nav>
+        <section className="nb__button-group">
+          <button onClick={async (e) => await dispatch(signOutRequest())}>
+            Sign Out
+          </button>
+        </section>
+      </section>
+    </div>
+  ) : null;
 }
