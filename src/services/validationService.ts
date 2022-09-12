794407f1d06b33825c86f4d3c9ab90fc79c54549
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-09-12 21:34:57 +0800

    Validation refactored

--- src/services/validationService.ts
+++ src/services/validationService.ts
@@ -6,96 +6,140 @@ export type Validation = {
   ) => [isError: boolean, errorMessage: string | null];
 };
 
+interface ValidationResult {
+  isError: boolean;
+  errorMessage: string;
+}
+
+const validateEmail = (inputValue: string): ValidationResult | any => {
+  const condition = !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g.test(
+    inputValue
+  );
+
+  return {
+    isError: condition ? true : false,
+    errorMessage: condition ? "Please enter a valid email address" : "",
+  };
+};
+
+const validateFirstName = (inputValue: string): ValidationResult | any => {
+  const condition =
+    !/^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/.test(inputValue);
+
+  return {
+    isError: condition ? true : false,
+    errorMessage: condition ? "Please enter a valid first name" : "",
+  };
+};
+
+const validateLastName = (inputValue: string): ValidationResult | any => {
+  const condition =
+    !/^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/.test(inputValue);
+
+  return {
+    isError: condition ? true : false,
+    errorMessage: condition ? "Please enter a valid last name" : "",
+  };
+};
+
+const validateUsername = (inputValue: string): ValidationResult | any => {
+  const condition =
+    !/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/.test(
+      inputValue
+    );
+
+  return {
+    isError: condition ? true : false,
+    errorMessage: condition ? "Username is not allowed" : "",
+  };
+};
+
+const validatePassword = (inputValue: string): ValidationResult | any => {
+  const condition =
+    !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+      inputValue
+    );
+
+  return {
+    isError: condition ? true : false,
+    errorMessage: condition
+      ? "Password must be minimum of eight characters, at least one uppercase letter, one lowercase letter and one number"
+      : "",
+  };
+};
+
+const validatePassword2 = (
+  inputValue: string,
+  secondValue: string
+): ValidationResult | any => {
+  if (inputValue === "") return;
+
+  const condition = inputValue !== secondValue;
+
+  return {
+    isError: condition ? true : false,
+    errorMessage: condition ? "Password didn't matched" : "",
+  };
+};
+
 export const validationService: Validation = {
   user: (type, inputValue, secondValue) => {
     let isError = false;
     let errorMessage: string | null = null;
 
     switch (type) {
-      case "email":
+      case "email": {
         if (inputValue === "") break;
 
-        if (
-          !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g.test(inputValue)
-        ) {
-          isError = true;
-          errorMessage = "Please enter a valid email address";
-          break;
-        }
+        const validation: ValidationResult = validateEmail(inputValue);
 
-        isError = false;
-        errorMessage = "";
+        isError = validation.isError;
+        errorMessage = validation.errorMessage;
         break;
-      case "firstName":
+      }
+      case "firstName": {
         if (inputValue === "") break;
-        if (
-          !/^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/.test(
-            inputValue
-          )
-        ) {
-          isError = true;
-          errorMessage = "Please enter a valid first name";
-          break;
-        }
-        isError = false;
-        errorMessage = "";
-        break;
-      case "lastName":
-        if (inputValue === "") break;
-        if (
-          !/^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/.test(
-            inputValue
-          )
-        ) {
-          isError = true;
-          errorMessage = "Please enter a valid last name";
-          break;
-        }
-        isError = false;
-        errorMessage = "";
-        break;
+        const validation: ValidationResult = validateFirstName(inputValue);
 
-      case "username":
+        isError = validation.isError;
+        errorMessage = validation.errorMessage;
+        break;
+      }
+      case "lastName": {
         if (inputValue === "") break;
+        const validation: ValidationResult = validateLastName(inputValue);
 
-        if (
-          !/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/.test(
-            inputValue
-          )
-        ) {
-          isError = true;
-          errorMessage = "Username is not allowed";
-          break;
-        }
-        isError = false;
-        errorMessage = "";
+        isError = validation.isError;
+        errorMessage = validation.errorMessage;
         break;
-      case "password":
+      }
+      case "username": {
         if (inputValue === "") break;
-        if (
-          !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
-            inputValue
-          )
-        ) {
-          isError = true;
-          errorMessage =
-            "Password must be minimum of eight characters, at least one uppercase letter, one lowercase letter and one number";
-          break;
-        }
-        isError = false;
-        errorMessage = "";
+        const validation: ValidationResult = validateUsername(inputValue);
+
+        isError = validation.isError;
+        errorMessage = validation.errorMessage;
         break;
-      case "password2":
+      }
+      case "password": {
         if (inputValue === "") break;
-        console.log(inputValue !== secondValue);
-        if (inputValue !== secondValue) {
-          isError = true;
-          errorMessage = "Password didn't matched";
-          break;
-        }
-        isError = false;
-        errorMessage = "";
+        const validation: ValidationResult = validatePassword(inputValue);
+
+        isError = validation.isError;
+        errorMessage = validation.errorMessage;
         break;
+      }
+      case "password2": {
+        if (inputValue === "") break;
+        const validation: ValidationResult = validatePassword2(
+          inputValue,
+          secondValue!
+        );
+
+        isError = validation.isError;
+        errorMessage = validation.errorMessage;
+        break;
+      }
       default:
         break;
     }
