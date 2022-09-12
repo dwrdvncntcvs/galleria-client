c3ba7140a663e70ce0bebd95271c50a87d455993
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-09-12 10:19:48 +0800

    Create post component created and minor chanes on home structuring

---
+++ src/components/Home/CreatePost/CreatePost.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./createPost.scss";
+
+export default function CreatePost() {
+  return <div>CreatePost</div>;
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { ContentContainer } from "../../components/global";
+import { CreatePost } from "../../components/Home";
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className="h__main-container">
       <ContentContainer>
-        <h1>Home</h1>
+        <section>
+          <h1>Home</h1>
+          <CreatePost />
+        </section>
+        <section>
+          <h1>Side</h1>
+        </section>
       </ContentContainer>
     </div>
   );
