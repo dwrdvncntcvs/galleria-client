dc4a9ddf68dff7ff036fff899fb051d34b02de42
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-09-13 19:57:59 +0800

    MInor changes on navbar

--- src/components/global/NavBar/NavBar.tsx
+++ src/components/global/NavBar/NavBar.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 import { signOutRequest } from "../../../api/userRequest";
 import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHook";
 import { HiPlus, HiLogout } from "react-icons/hi";
 import "./navBar.scss";
 import NavLinks from "../NavLinks/NavLinks";
+import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
   const { userState } = useAppSelector((state) => state);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const signOutAction = async (e: MouseEvent<HTMLButtonElement>) => {
+    await dispatch(signOutRequest());
+    navigate("/");
+  };
 
   return userState.isAuth ? (
     <div className="nb__main-container">
@@ -21,7 +28,7 @@ export default function NavBar() {
             <HiPlus size={15} />
           </button>
           <NavLinks user={userState.userData!} />
-          <button onClick={async (e) => await dispatch(signOutRequest())}>
+          <button onClick={signOutAction}>
             <HiLogout size={15} />
           </button>
         </section>
