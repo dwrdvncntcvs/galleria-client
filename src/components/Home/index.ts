d5a5ba8b166af42a9244a4dc6c043aaf7ae1daf8
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-09-13 20:35:44 +0800

    Create Post Modal Component Added

--- src/components/Home/index.ts
+++ src/components/Home/index.ts
@@ -2,3 +2,4 @@ export { default as CreatePost } from "./CreatePost/CreatePost";
 export { default as Posts } from "./Posts/Posts";
 export { default as PostCard } from "./PostCard/PostCard";
 export { default as SidePanel } from "./SidePanel/SidePanel";
+export { default as CreatePostModal } from "./CreatePostModal/CreatePostModal";
