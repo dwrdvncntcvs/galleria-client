ee93a234a9c2b0503037004c85242d2a85a671a8
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-09-24 21:00:51 +0800

    Minor changes

--- src/components/global/InfiniteScroll/InfiniteScroll.tsx
+++ src/components/global/InfiniteScroll/InfiniteScroll.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useEffect, useCallback } from "react";
+import React, { PropsWithChildren, useEffect } from "react";
 import { changePage, setHasMore } from "../../../features/postSlice";
 import { useAppDispatch } from "../../../hooks/reduxHook";
 import { throttle } from "lodash";
@@ -35,29 +35,24 @@ const InfiniteScroll = ({
     await dispatch(dataRequest({ userId, limit, page }));
   };
 
-  const handleScrollChange = useCallback(
-    throttle(() => {
-      const isAtBottom =
-        document.documentElement.scrollHeight -
-          document.documentElement.scrollTop <=
-        document.documentElement.clientHeight;
+  const handleScrollChange = throttle(() => {
+    const isAtBottom =
+      document.documentElement.scrollHeight -
+        document.documentElement.scrollTop <=
+      document.documentElement.clientHeight;
 
-      if (Math.ceil(numberOfItems / limit) <= page) {
-        dispatch(setHasMore(false));
-        return;
-      }
+    if (Math.ceil(numberOfItems / limit) <= page) {
+      dispatch(setHasMore(false));
+      return;
+    }
 
-      if (isAtBottom) {
-        dispatch(changePage(page + 1));
-        request();
-      }
-    }, 1000),
-    [numberOfItems, numberOfItems, page]
-  );
+    if (isAtBottom) {
+      dispatch(changePage(page + 1));
+      request();
+    }
+  }, 700);
 
   useEffect(() => {
-    console.log("Current Page: ", page);
-
     window.addEventListener("scroll", handleScrollChange);
 
     return () => {
