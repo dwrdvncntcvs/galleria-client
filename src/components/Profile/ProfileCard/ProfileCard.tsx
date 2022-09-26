9b34b53b5f25da53eb6eb8c99b85f3068503c501
Reneil Moncawe <reneil.moncawe@g.batstate-u.edu.ph>
2022-09-26 17:33:55 +0800

    proile initial design

--- src/components/Profile/ProfileCard/ProfileCard.tsx
+++ src/components/Profile/ProfileCard/ProfileCard.tsx
@@ -9,21 +9,37 @@ interface ProfileCardProps {
 
 export default function ProfileCard({ profile }: ProfileCardProps) {
   return (
-    <div className={style.content}>
-      <img
-        src={
-          profile.Profile?.profileImage === ""
-            ? defaultAvatar
-            : profile.Profile?.profileImage
-        }
-        alt={`${profile.first_name}'s avatar'`}
-      />
-      <h1>
-        {profile.first_name}
-        {profile.last_name}
-      </h1>
-      <h3>{profile.email}</h3>
-      <h3>{profile.username}</h3>
+    <div className={style["main-container"]}>
+      <div className={style.profilebg}>{/* img src background here */}</div>
+      <div className={style["profile-container"]}>
+        <div className={style.profilelink}>
+          <img
+            src={
+              profile.Profile?.profileImage === ""
+                ? defaultAvatar
+                : profile.Profile?.profileImage
+            }
+            alt={`${profile.first_name}'s avatar'`}
+          />
+        </div>
+        <button className={style["follow-btn"]}>Follow</button>
+      </div>
+      <div className={style["profile-info"]}>
+        <h4>
+          {profile.first_name}&nbsp;
+          {profile.last_name}
+        </h4>
+        <p className={style["username-txt"]}>{profile.username}</p>
+        <div className={style["follow-container"]}>
+          <p className={style["follow-txt"]}>
+            30 <span> Following </span>
+          </p>
+          &nbsp;&nbsp;
+          <p className={style["follow-txt"]}>
+            28 <span> Followers </span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
