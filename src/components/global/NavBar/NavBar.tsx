27f3b99c30a7480221bc4b51b33dcc15c234d8a3
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-10-01 11:04:50 +0800

    Conditional changes in navbar

--- src/components/global/NavBar/NavBar.tsx
+++ src/components/global/NavBar/NavBar.tsx
@@ -4,21 +4,34 @@ import style from "./navBar.module.scss";
 import NavLinks from "../NavLinks/NavLinks";
 
 import { stopPropagation } from "../../../utils/helper";
+import { useLocation } from "react-router-dom";
 
 export default function NavBar() {
   const { userState } = useAppSelector((state) => state);
+  const location = useLocation();
 
-  return userState.isAuth ? (
+  if (location.pathname === "/") return null;
+
+  return (
     <div className={style["nav-container"]}>
       <section className={style["nav-content"]}>
         <section className={style["title-container"]}>
           <h1>Galleria</h1>
         </section>
-        <input type="text" placeholder="Galleria Search ..." />
-        <section className={style["nav-btn-group"]} onClick={stopPropagation}>
-          <NavLinks user={userState.userData!} />
-        </section>
+        {userState.isAuth ? (
+          <>
+            <input type="text" placeholder="Galleria Search ..." />
+            <section
+              className={style["nav-btn-group"]}
+              onClick={stopPropagation}
+            >
+              <NavLinks user={userState.userData!} />
+            </section>
+          </>
+        ) : (
+          <>Hello </>
+        )}
       </section>
     </div>
-  ) : null;
+  );
 }
