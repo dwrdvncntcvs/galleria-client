10f9aaa898658d50555bbfde8999120dd9103b14
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-10-03 16:43:05 +0800

    Previous commit doesn't work properly lol

--- src/components/Post/EditPostModal/EditPostModal.tsx
+++ src/components/Post/EditPostModal/EditPostModal.tsx
@@ -1,21 +1,11 @@
 import React from "react";
-import { useAppSelector } from "../../../hooks/reduxHook";
 import { ModalOverlay } from "../../../layouts";
 import style from "./editPostModal.module.scss";
 
 export default function EditPostModal() {
-  const { posts } = useAppSelector((state) => state.postState);
-  const { props } = useAppSelector((state) => state.modalState);
-
-  const { postId } = props as { postId: string };
-
-  const post = posts.filter((post) => post.id === postId)[0];
-
-  console.log("Post: ", post);
-
   return (
     <ModalOverlay>
-      <div>EditPostModal</div>
+      <div>Edit Post</div>
     </ModalOverlay>
   );
 }
