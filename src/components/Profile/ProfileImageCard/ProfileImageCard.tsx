901a4db043ec61665604f73ada7eb8cd5c57faed
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-10-03 13:39:53 +0800

    Minor changes

--- src/components/Profile/ProfileImageCard/ProfileImageCard.tsx
+++ src/components/Profile/ProfileImageCard/ProfileImageCard.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import style from "./profileImageCard.module.scss";
 import { UserProfile } from "../../../models/User";
 import { defaultAvatar } from "../../../assets/images";
+import { useAppSelector } from "../../../hooks/reduxHook";
 
 interface ProfileImageCardProps {
   profile: UserProfile;
 }
 
 export default function ProfileImageCard({ profile }: ProfileImageCardProps) {
+  const { userData } = useAppSelector((state) => state.userState);
   return (
     <div className={style["profile-container"]}>
       <div className={style["profile-link"]}>
@@ -20,13 +22,9 @@ export default function ProfileImageCard({ profile }: ProfileImageCardProps) {
           alt={`${profile.first_name}'s avatar'`}
         />
       </div>
-      {/* 
-      This "follow" button should be dynamically displayed if certain conditions were met. 
-      Condition: 
-          If currently logged in user id is equal to this user profile's id, then don't display this button.
-          Else display this button
-      */}
-      <button className={style["follow-btn"]}>Follow</button>{" "}
+      {userData?.id !== profile.id && (
+        <button className={style["follow-btn"]}>Follow</button>
+      )}
     </div>
   );
 }
