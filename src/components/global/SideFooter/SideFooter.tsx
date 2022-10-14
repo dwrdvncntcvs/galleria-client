7b707fbed7d099fd809c559e404f206e267a1990
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-10-14 16:02:21 +0800

    Side footer created

--- src/components/global/SideFooter/SideFooter.tsx
+++ src/components/global/SideFooter/SideFooter.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
 import style from "./sideFooter.module.scss";
 
+type Link = {
+  label: string;
+  link: string;
+};
+
 export default function SideFooter() {
-  return <div>SideFooter</div>;
+  const links: Link[] = [
+    { link: "#", label: "Terms of Service" },
+    { link: "#", label: "Privacy Policy" },
+    { link: "#", label: "Cookie Policy" },
+    { link: "#", label: "About" },
+  ];
+
+  return (
+    <div className={style["side-footer"]}>
+      {links.map(({ label, link }, i) => (
+        <Fragment key={i}>
+          <a href={link}>{label}</a> {links.length - 1 !== i ? "|" : ""}{" "}
+        </Fragment>
+      ))}...
+      <p>@2022 Galleria - All Rights Reserved.</p>
+    </div>
+  );
 }
