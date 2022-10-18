e335b16f50a1cb0ea8c1139df5cdc8bf436c864f
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-10-18 13:27:30 +0800

    Buttons added

--- src/components/Profile/ImageAction/ImageAction.tsx
+++ src/components/Profile/ImageAction/ImageAction.tsx
@@ -3,11 +3,29 @@ import { Dropdown } from "../../../UI";
 import style from "./imageAction.module.scss";
 
 export default function ImageAction() {
+  const buttons = [
+    {
+      label: "View",
+      action: () => {
+        console.log("View");
+      },
+    },
+    {
+      label: "Update",
+      action: () => {
+        console.log("Update");
+      },
+    },
+  ];
+
   return (
     <Dropdown>
       <div className={style["image-action"]}>
-        <button>View</button>
-        <button>Update</button>
+        {buttons.map(({ label, action }, i) => (
+          <button onClick={action} key={i}>
+            {label}
+          </button>
+        ))}
       </div>
     </Dropdown>
   );
