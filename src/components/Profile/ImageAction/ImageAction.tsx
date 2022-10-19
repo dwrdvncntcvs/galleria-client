5397ab62cc538428c3ffd7536a1958dd01935403
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-10-19 08:43:49 +0800

    Minor changes

--- src/components/Profile/ImageAction/ImageAction.tsx
+++ src/components/Profile/ImageAction/ImageAction.tsx
@@ -1,16 +1,26 @@
 import React from "react";
+import { HiEye, HiPencilAlt } from "react-icons/hi";
+import { IconType } from "react-icons/lib";
 import { Dropdown } from "../../../UI";
 import style from "./imageAction.module.scss";
 
+type ImageActionButtons = {
+  Icon: IconType;
+  label: string;
+  action: () => void;
+};
+
 export default function ImageAction() {
-  const buttons = [
+  const buttons: ImageActionButtons[] = [
     {
+      Icon: HiEye,
       label: "View",
       action: () => {
         console.log("View");
       },
     },
     {
+      Icon: HiPencilAlt,
       label: "Update",
       action: () => {
         console.log("Update");
@@ -21,8 +31,9 @@ export default function ImageAction() {
   return (
     <Dropdown>
       <div className={style["image-action"]}>
-        {buttons.map(({ label, action }, i) => (
+        {buttons.map(({ label, action, Icon }, i) => (
           <button onClick={action} key={i}>
+            <Icon />
             {label}
           </button>
         ))}
